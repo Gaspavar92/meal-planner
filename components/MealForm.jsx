1985a46b06ad387112bd48aa415121c129cf4a80
Gaspare Tortora <g.tortora2@gmail.com>
2023-11-06 16:07:34 +0100

    Created MealForm component

--- components/MealForm.jsx
+++ components/MealForm.jsx
@@ -1,10 +1,11 @@
+import styles from "./styling/MealForm.module.css";
+
 const MealForm = () => {
     return (
-        <select name="meal" id="meal">
-            <option value="meal-1">Meal 1</option>
-            <option value="meal-2">Meal 2</option>
-            <option value="meal-3">Meal 3</option>
-        </select>
+        <>
+            <label htmlFor="meal">Meal</label>
+            <input type="text" name="meal" id="meal" />
+        </>
     )
 };
 
