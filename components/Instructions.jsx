3a693b3684986166e54f1c3f3c9a0c73d0207a50
Gaspare Tortora <g.tortora2@gmail.com>
2023-11-06 16:29:47 +0100

    Created Instructions component

---
+++ components/Instructions.jsx
@@ -0,0 +1,9 @@
+const Instructions = () => {
+    return (
+        <div className="instructions">
+            <p>These are some instructions</p>
+        </div>
+    )
+}
+
+export default Instructions;
