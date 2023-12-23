b781a35fdc8cfe079c6ac51f3c1215e2205cca14
Gaspare Tortora <g.tortora2@gmail.com>
2023-12-23 11:35:36 -0500

    Created and styled Meal component

--- components/Meal.jsx
+++ components/Meal.jsx
@@ -39,9 +39,40 @@ const Meal = ({query}) => {
         fetchData()
     }, [query])
 
+    if (!query) {
+        return (
+            <div className={styles.center}>
+                <i className={`fa-solid fa-arrow-up ${styles.arrow_up}`}></i>
+                <p>Look for a meal and check its nutritional value.</p>
+            </div>
+        )
+    }
+
     return (
-        <>
-        </>
+        <div className={styles.meal_name}>
+            {loading ?
+            "Loading..." :
+            <div className={styles.meal_card}>
+                {response.length === 0 ?
+                "No results found. Please try again." :
+                response[0]?.name &&
+                <>
+                    <h2>{response[0]?.name.split('')[0].toUpperCase() + response[0]?.name.slice(1) + ` (${response[0]?.serving_size_g}g)`}</h2>
+                    <div className={styles.meal_details}>
+                        <li>{`Calories: ${response[0]?.calories}`}</li>
+                        <li>{`Carbohydrates: ${response[0]?.carbohydrates_total_g}g`}</li>
+                        <li>{`Cholesterol: ${response[0]?.cholesterol_mg}mg`}</li>
+                        <li>{`Saturated fats: ${response[0]?.fat_saturated_g}g`}</li>
+                        <li>{`Total fats: ${response[0]?.fat_total_g}g`}</li>
+                        <li>{`Potassium: ${response[0]?.potassium_mg}mg`}</li>
+                        <li>{`Protein: ${response[0]?.protein_g}g`}</li>
+                        <li>{`Sodium: ${response[0]?.sodium_mg}mg`}</li>
+                        <li>{`Sugar: ${response[0]?.sugar_g}g`}</li>
+                    </div>
+                </>}
+            </div>
+            }
+        </div>
     )
 };
 
